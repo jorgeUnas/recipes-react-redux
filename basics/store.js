a10ab08b82f5e01da059e577a22ca1e3ece42791
EC2 Default User <ec2-user@ip-172-31-43-56.eu-north-1.compute.internal>
2024-04-04 15:50:05 +0000

    add action creators for the searchTerm slice

--- basics/store.js
+++ basics/store.js
@@ -3,3 +3,39 @@ const initialState = {
   favoriteRecipes: [],
   searchTerm: ''
 };
+
+// Dispatched when the user types in the search input.
+// Sends the search term to the store.
+export const setSearchTerm = (term) => {
+  return { 
+    type: 'searchTerm/setSearchTerm', 
+    payload: term 
+  };
+}
+
+// Dispatched when the user presses the clear search button.
+export const clearSearchTerm = () => {
+  return { 
+    type: 'searchTerm/clearSearchTerm' 
+  };
+}
+
+// Dispatched when the user first opens the application.
+// Sends the allRecipesData array to the store.
+export const loadData = () => {
+
+}
+
+// Dispatched when the user clicks on the heart icon of 
+// a recipe in the "All Recipes" section.
+// Sends the recipe object to the store.
+export const addRecipe = (recipe) => {
+
+}
+
+// Dispatched when the user clicks on the broken heart 
+// icon of a recipe in the "Favorite Recipes" section.
+// Sends the recipe object to the store.
+export const removeRecipe = (recipe) => {
+
+}

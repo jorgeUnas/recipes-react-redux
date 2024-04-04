03493e35d6933f7834e47d99d33056a04837a3e9
EC2 Default User <ec2-user@ip-172-31-43-56.eu-north-1.compute.internal>
2024-04-04 10:51:58 +0000

    State model for the recipe app

---
+++ basics/state.js
@@ -0,0 +1,11 @@
+const state = {
+  allRecipes: [
+    {id: 0, name: 'Jjampong', img: 'img/jjampong.png' },
+    {id: 2, name: 'Cheeseburger', img: 'img/cheeseburger.png' },
+    //… more recipes omitted
+  ],
+  favoriteRecipes: [
+    {id: 1, name: 'Doro Wat', img: 'img/doro-wat.png' },
+  ],
+  searchTerm: 'Doro'
+};

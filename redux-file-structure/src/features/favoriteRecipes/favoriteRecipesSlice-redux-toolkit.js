fa1633cfadf1943547e416830d47d68582604f49
EC2 Default User <ec2-user@ip-172-31-43-56.eu-north-1.compute.internal>
2024-04-08 13:45:38 +0000

    Create a options object that will be the argument for createSlice

--- redux-file-structure/src/features/favoriteRecipes/favoriteRecipesSlice-redux-toolkit.js
+++ redux-file-structure/src/features/favoriteRecipes/favoriteRecipesSlice-redux-toolkit.js
@@ -1,1 +1,22 @@
 import { createSlice }  from '@reduxjs/toolkit';
+import { selectSearchTerm } from '../searchTerm/searchTermSlice.js';
+
+/* Create your Slice object here. */
+const options = {
+  name: 'favoriteRecipes',
+  initialState: [], 
+  reducers: {}
+};
+
+/* Do not delete the code below...*/
+
+export const selectFavoriteRecipes = (state) => state.favoriteRecipes;
+
+export const selectFilteredFavoriteRecipes = (state) => {
+  const favoriteRecipes = selectFavoriteRecipes(state);
+  const searchTerm = selectSearchTerm(state);
+
+  return favoriteRecipes.filter((recipe) =>
+    recipe.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+};
